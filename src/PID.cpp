99d293f3f94d5882e10e63915cc0734abcc309c4
William Fallas <wfc@hpe.com>
2017-08-02 20:33:31 -0600

    Update PID error

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,24 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_init, double Ki_init, double Kd_init) {
+    Kp = Kp_init;
+    Kd = Kd_init;
+    Ki = Ki_init;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+    prev_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+double PID::UpdateError(double cte) {
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - prev_error;
+    prev_error = cte;
+
+    return (-Kp * p_error - Kd * d_error - Ki * i_error);
 }
 
-double PID::TotalError() {
-}
-
+double PID::TotalError() {}
